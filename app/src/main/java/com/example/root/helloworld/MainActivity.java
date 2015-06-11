8c13b03b0fefef2525bd1726b9eae993954a1463
Ahmed Osman <eng.ahmeda91@gmail.com>
2015-06-11 20:11:01 +0300

    Applying the points i had not do it right, with my first submittion
    --factoring out the dimentions, and margins of buttons
    --using appropiate names for buttons id
    --using onClick XML method by creating Public function to handle the event
    --instead of hard coding the name apper in Toast message, just fetch it from TextView Element

--- app/src/main/java/com/example/root/helloworld/MainActivity.java
+++ app/src/main/java/com/example/root/helloworld/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.root.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -20,90 +21,96 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button1 = (Button) findViewById(R.id.button1);
-        Button button2 = (Button) findViewById(R.id.button2);
-        Button button3 = (Button) findViewById(R.id.button3);
-        Button button4 = (Button) findViewById(R.id.button4);
-        Button button5 = (Button) findViewById(R.id.button5);
-        Button button6 = (Button) findViewById(R.id.button6);
+      }
 
-        button1.setOnClickListener(new OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                toasttext = "SPOTIFY STREAMER";
-                toast = Toast.makeText(getApplicationContext(), toasttext, duration);
-                toast.show();
-            }
-        });
-
-
-
-        button2.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                toasttext = "SCORES APP";
-                toast = Toast.makeText(getApplicationContext(), toasttext,duration);
-                toast.show();
-            }
-        });
-
-        button3.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                toasttext = "LIBRARY APP";
-                toast = Toast.makeText(getApplicationContext(), toasttext,duration);
-                toast.show();
-            }
-        });
-
-        button4.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                toasttext = "BUILD IT BIGGER";
-                toast = Toast.makeText(getApplicationContext(), toasttext,duration);
-                toast.show();
-            }
-        });
-
-        button5.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                toasttext = "XYZ READER";
-                toast = Toast.makeText(getApplicationContext(), toasttext,duration);
-                toast.show();
-            }
-        });
-
-        button6.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                toasttext = "Capstone Project";
-                toast = Toast.makeText(getApplicationContext(), toasttext,duration);
-                toast.show();
-            }
-        });
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void showToast(View v)
+    {
+        switch (v.getId())
+        {
+            case R.id.spotify:
+            case R.id.scores:
+            case R.id.library:
+            case R.id.builditbigger:
+            case R.id.xyzreader:
+            case R.id.caproject:
+                displayToast(((Button) v).getText().toString());
+                //Log.d("Buttontext",((Button) v).getText().toString() );
+            default:
+                break;
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
+    private void displayToast(String s) {
+        if (toast != null)
+        {
+            toast.cancel();
+        }
+        toast = Toast.makeText(getApplicationContext(),s,duration);
+        toast.show();
+    }
+
+//Un Professional way in this case
+//        Button button1 = (Button) findViewById(R.id.button1);
+//        Button button2 = (Button) findViewById(R.id.button2);
+//        Button button3 = (Button) findViewById(R.id.button3);
+//        Button button4 = (Button) findViewById(R.id.button4);
+//        Button button5 = (Button) findViewById(R.id.button5);
+//        Button button6 = (Button) findViewById(R.id.button6);
+//
+//        button1.setOnClickListener(new OnClickListener(){
+//            @Override
+//            public void onClick(View v) {
+//                toasttext = "SPOTIFY STREAMER";
+//                toast = Toast.makeText(getApplicationContext(), toasttext, duration);
+//                toast.show();
+//            }
+//        });
+//
+//
+//
+//        button2.setOnClickListener(new OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                toasttext = "SCORES APP";
+//                toast = Toast.makeText(getApplicationContext(), toasttext,duration);
+//                toast.show();
+//            }
+//        });
+//
+//        button3.setOnClickListener(new OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                toasttext = "LIBRARY APP";
+//                toast = Toast.makeText(getApplicationContext(), toasttext,duration);
+//                toast.show();
+//            }
+//        });
+//
+//        button4.setOnClickListener(new OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                toasttext = "BUILD IT BIGGER";
+//                toast = Toast.makeText(getApplicationContext(), toasttext,duration);
+//                toast.show();
+//            }
+//        });
+//
+//        button5.setOnClickListener(new OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                toasttext = "XYZ READER";
+//                toast = Toast.makeText(getApplicationContext(), toasttext,duration);
+//                toast.show();
+//            }
+//        });
+//
+//        button6.setOnClickListener(new OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                toasttext = "Capstone Project";
+//                toast = Toast.makeText(getApplicationContext(), toasttext,duration);
+//                toast.show();
+//            }
+//        });
+
 }
